e436e580a64974d048fb589c624ac876c9888d07
安藤春香 <haruan2394@gmail.com>
2018-09-02 23:46:28 +0900

    add Model

--- src/main.rs
+++ src/main.rs
@@ -7,70 +7,78 @@ struct User {
   age: u32,
 }
 
+struct Model {
+    users : Vec<User>
+}
+
 impl User {
     fn new_user(name: String, age: u32) -> User {
         User { name, age }
     }
 }
 
-fn show(users: &Vec<User>) {
-    println!("{}","This is users".green().bold());
-    println!("{}","-------------".blue().bold());
-    for (i,user) in users.into_iter().enumerate() {
-        println!("id: {}",i);
-        println!("name: {}",user.name);
-        println!("age:{}",user.age);
+impl Model {
+    fn show(&mut self) {
+        println!("{}","This is users".green().bold());
         println!("{}","-------------".blue().bold());
+        for (i,user) in self.users.iter().enumerate() {
+            println!("id: {}",i);
+            println!("name: {}",user.name);
+            println!("age:{}",user.age);
+            println!("{}","-------------".blue().bold());
+        }
     }
+
+    fn new(&mut self) {
+        println!("{}","Please type a name!".yellow().bold());
+        let mut name = String::new();
+        io::stdin().read_line(&mut name)
+            .expect("Failed to read line");
+        println!("{}","Please type a age".yellow().bold());
+        let mut age = String::new();
+        io::stdin().read_line(&mut age)
+            .expect("Failed to read line");
+        self.users.push(User::new_user(name.trim().to_string(),age.trim().parse::<u32>().unwrap()));
+    }
+
+    fn delete(&mut self) {
+        println!("{}","Which user do you delete?".white().bold());
+        self.show();
+        println!("{}","Please type the id which you want to delete".yellow().bold());
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+        let id: usize = input.trim().parse().unwrap();
+        self.users.remove(id);
+    }
+
+    fn update(&mut self) {
+        println!("{}","Which user do you update?".white().bold());
+        self.show();
+        println!("{}","Please type the id which you want to update".yellow().bold());
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+        let id: usize = input.trim().parse().unwrap();
+        println!("{}","Please edit the user".yellow().bold());
+        println!("{}","Please type a name!".yellow().bold());
+        println!("previous name: {}",self.users[id].name);
+        let mut name = String::new();
+        io::stdin().read_line(&mut name)
+            .expect("Failed to read line");
+        println!("{}","Please type a age".yellow().bold());
+        println!("previous name: {}",self.users[id].age);
+        let mut age = String::new();
+        io::stdin().read_line(&mut age)
+            .expect("Failed to read line");
+        self.users.insert(id,User::new_user(name.trim().to_string(), age.trim().parse::<u32>().unwrap()));
+    }
+
 }
 
-fn new(users: &mut Vec<User>) {
-    println!("{}","Please type a name!".yellow().bold());
-    let mut name = String::new();
-    io::stdin().read_line(&mut name)
-        .expect("Failed to read line");
-    println!("{}","Please type a age".yellow().bold());
-    let mut age = String::new();
-    io::stdin().read_line(&mut age)
-        .expect("Failed to read line");
-    users.push(User::new_user(name.trim().to_string(),age.trim().parse::<u32>().unwrap()));
-}
-
-fn delete(users: &mut Vec<User>) {
-    println!("{}","Which user do you delete?".white().bold());
-    show(users);
-    println!("{}","Please type the id which you want to delete".yellow().bold());
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line");
-    let id: usize = input.trim().parse().unwrap();
-    users.remove(id);
-}
-
-fn update(users: &mut Vec<User>) {
-    println!("{}","Which user do you update?".white().bold());
-    show(users);
-    println!("{}","Please type the id which you want to update".yellow().bold());
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line");
-    let id: usize = input.trim().parse().unwrap();
-    println!("{}","Please edit the user".yellow().bold());
-    println!("{}","Please type a name!".yellow().bold());
-    println!("previous name: {}",users[id].name);
-    let mut name = String::new();
-    io::stdin().read_line(&mut name)
-        .expect("Failed to read line");
-    println!("{}","Please type a age".yellow().bold());
-    println!("previous name: {}",users[id].age);
-    let mut age = String::new();
-    io::stdin().read_line(&mut age)
-        .expect("Failed to read line");
-    users.insert(id,User::new_user(name.trim().to_string(), age.trim().parse::<u32>().unwrap()));
-}
 
 fn main() {
-    let mut users = Vec::new();
+    let mut model = Model { users: Vec::new() };
     println!("{}","Hello! This is a name list!\n".red().bold());
     loop {
         println!("{}","Please type a number\n".yellow().bold());
@@ -87,10 +95,10 @@ fn main() {
             Err(_) => {println!("{}","Cannot parse".red().bold());continue},
         };
         match input_number {
-            0 => new(&mut users),
-            1 => show(&users),
-            2 => delete(&mut users),
-            3 => update(&mut users),
+            0 => model.new(),
+            1 => model.show(),
+            2 => model.delete(),
+            3 => model.update(),
             4 => {println!("{}","Good Bye!".red().bold());break},
             _ => println!("{}","this is not a number".red().bold())
         }
